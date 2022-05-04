17131e023191e7c56883a2875a373a1f82549262
Gaëlle CHALOT <chalot.g@sfeir.com>
2022-05-04 11:35:31 +0200

    Add fastify and swagger

---
+++ apps/api/src/app/models/message.dto.ts
@@ -0,0 +1,8 @@
+import { Message } from '@sic/api-interfaces';
+
+export class MyMessage implements Message {
+  /**
+   * The message.
+   */
+  message: string;
+}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -5,15 +5,35 @@
 
 import { Logger } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app/app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    new FastifyAdapter()
+  );
+
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
+
+  const config = new DocumentBuilder()
+    .setTitle('Cats example')
+    .setDescription('The cats API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, document);
+
   const port = process.env.PORT || 3333;
   await app.listen(port);
+
   Logger.log(
     `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
   );
