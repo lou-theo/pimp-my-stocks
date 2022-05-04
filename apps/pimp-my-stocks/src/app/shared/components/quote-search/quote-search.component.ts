ed7602b3ce01ba68939c64bc3ff12c1cf12be4a0
Gaëlle CHALOT <chalot.g@sfeir.com>
2022-05-04 17:54:05 +0200

    Add quote search

--- apps/pimp-my-stocks/src/app/shared/components/quote-search/quote-search.component.ts
+++ apps/pimp-my-stocks/src/app/shared/components/quote-search/quote-search.component.ts
@@ -1,4 +1,21 @@
-import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+import {
+    Component,
+    OnInit,
+    ChangeDetectionStrategy,
+    OnDestroy,
+} from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { SearchQuoteDto } from '@sic/core/api/models';
+import { ApiService } from '@sic/core/api/services';
+import { notUndefined } from '@sic/core/utils/not-undefined';
+import {
+    BehaviorSubject,
+    debounceTime,
+    distinctUntilChanged,
+    firstValueFrom,
+    Observable,
+    Subscription,
+} from 'rxjs';
 
 @Component({
     selector: 'sic-quote-search',
@@ -6,8 +23,40 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
     styleUrls: ['./quote-search.component.scss'],
     changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class QuoteSearchComponent implements OnInit {
-    constructor() {}
+export class QuoteSearchComponent implements OnInit, OnDestroy {
+    public searchControl: FormControl = new FormControl('');
 
-    ngOnInit(): void {}
+    private quotes: BehaviorSubject<string[]> = new BehaviorSubject<string[]>(
+        []
+    );
+    public quotes$: Observable<string[]> = this.quotes.asObservable();
+
+    private subscriptions: Subscription = new Subscription();
+
+    constructor(private readonly apiService: ApiService) {}
+
+    ngOnInit(): void {
+        this.subscriptions.add(
+            this.searchControl.valueChanges
+                .pipe(distinctUntilChanged(), debounceTime(500))
+                .subscribe(async (searchValue: string) => {
+                    await this.onSearchValueChange(searchValue);
+                })
+        );
+    }
+
+    private async onSearchValueChange(value: string): Promise<void> {
+        const searchResult: SearchQuoteDto[] = await firstValueFrom(
+            this.apiService.yahooControllerSearch({ query: value })
+        );
+
+        const quotes: string[] = searchResult
+            .map((r) => r.longname)
+            .filter(notUndefined);
+        this.quotes.next(quotes);
+    }
+
+    ngOnDestroy(): void {
+        this.subscriptions.unsubscribe();
+    }
 }
--- apps/pimp-my-stocks/src/app/shared/shared.module.ts
+++ apps/pimp-my-stocks/src/app/shared/shared.module.ts
@@ -3,10 +3,18 @@ import { CommonModule } from '@angular/common';
 import { ChartComponent } from './components/chart/chart.component';
 import { QuoteSearchComponent } from './components/quote-search/quote-search.component';
 import { QuoteDetailsComponent } from './components/quote-details/quote-details.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [ChartComponent, QuoteSearchComponent, QuoteDetailsComponent],
-    imports: [CommonModule],
-    exports: [ChartComponent, QuoteSearchComponent, QuoteDetailsComponent],
+    imports: [CommonModule, FormsModule, ReactiveFormsModule],
+    exports: [
+        CommonModule,
+        FormsModule,
+        ReactiveFormsModule,
+        ChartComponent,
+        QuoteSearchComponent,
+        QuoteDetailsComponent,
+    ],
 })
 export class SharedModule {}
