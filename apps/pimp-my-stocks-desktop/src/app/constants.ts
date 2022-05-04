71bb2f03969338e4fe1338544cf7c6070ad50d19
Gaëlle CHALOT <chalot.g@sfeir.com>
2022-05-04 10:47:30 +0200

    Add electron project

---
+++ apps/pimp-my-stocks-desktop/src/app/constants.ts
@@ -0,0 +1,4 @@
+export const rendererAppPort = 4200;
+export const rendererAppName = 'pimp-my-stocks';
+export const electronAppName = 'pimp-my-stocks-desktop';
+export const updateServerUrl = 'https://deployment-server-url.com'; // TODO: insert your update server url here
