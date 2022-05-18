00a09fa794f697b746149899145bf026483c9824
Gaëlle CHALOT <chalot.g@sfeir.com>
2022-05-18 09:32:23 +0200

    Edit condition creation form

--- apps/pimp-my-stocks/src/app/modules/shared/components/edit-condition/edit-condition.component.ts
+++ apps/pimp-my-stocks/src/app/modules/shared/components/edit-condition/edit-condition.component.ts
@@ -1,6 +1,27 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
-import { AggregationTypeLabelMapping } from '@sic/core/conditions';
+import {
+    AbstractControl,
+    FormBuilder,
+    FormControl,
+    FormGroup,
+    Validators,
+} from '@angular/forms';
+import {
+    AggregationType,
+    AggregationTypeLabelMapping,
+    EqualityType,
+    EqualityTypeLabelMapping,
+} from '@sic/core/conditions';
 import { IndicatorService } from '@sic/core/services';
+
+type ConditionForm = {
+    aggregationType: AggregationType;
+    indicatorIndex: number;
+    range: number;
+    equalityType: EqualityType;
+    comparedValue: number;
+};
+
 @Component({
     selector: 'sic-edit-condition',
     templateUrl: './edit-condition.component.html',
@@ -9,6 +30,27 @@ import { IndicatorService } from '@sic/core/services';
 })
 export class EditConditionComponent {
     AggregationTypeLabelMapping = AggregationTypeLabelMapping;
+    EqualityTypeLabelMapping = EqualityTypeLabelMapping;
 
-    constructor(public readonly indicatorService: IndicatorService) {}
+    public conditionFormGroup: FormGroup;
+
+    public getControlFor(controlName: keyof ConditionForm): FormControl {
+        return this.conditionFormGroup.get(controlName) as FormControl;
+    }
+
+    constructor(
+        public readonly indicatorService: IndicatorService,
+        public readonly fb: FormBuilder
+    ) {
+        const formControls: { [key in keyof ConditionForm]: AbstractControl } =
+            {
+                aggregationType: fb.control(null, Validators.required),
+                indicatorIndex: fb.control(null, Validators.required),
+                range: fb.control(null, Validators.required),
+                equalityType: fb.control(null, Validators.required),
+                comparedValue: fb.control(null, Validators.required),
+            };
+
+        this.conditionFormGroup = fb.group(formControls);
+    }
 }
